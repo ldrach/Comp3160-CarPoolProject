af626a871e60cde037fb54f9cf4d3692a7dc52f2
shane <steiner5676@gmail.com>
2020-01-19 18:52:54 -0800

    Main Activity basic listview
    I set up a basic listview with a adapter.

--- app/src/main/java/com/example/carpoolapp/MainActivity.java
+++ app/src/main/java/com/example/carpoolapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.carpoolapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //this sets up the values for teh list view
+    ListView list;
+    String[] maintitle ={
+            "Title 1","Title 2",
+            "Title 3","Title 4",
+            "Title 5",
+    };
+
+    String[] subtitle ={
+            "Sub Title 1","Sub Title 2",
+            "Sub Title 3","Sub Title 4",
+            "Sub Title 5",
+    };
+    Integer[] imgid={
+            R.drawable.icon_1,R.drawable.icon_1,R.drawable.icon_1,R.drawable.icon_1,R.drawable.icon_1,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //----this code sets up an adapter for the list view
+        mainActivityListAdapter adapter=new mainActivityListAdapter(this, maintitle, subtitle,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        //----
     }
 }
