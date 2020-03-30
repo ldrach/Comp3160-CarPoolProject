74309eda0e75dc727dc6005cd18e62c03526d683
shane <steiner5676@gmail.com>
2020-03-29 17:32:49 -0700

    Finished updating drivecount
    still needs to be tested

--- app/src/main/java/com/example/carpoolapp/UpdateDriveCount.java
+++ app/src/main/java/com/example/carpoolapp/UpdateDriveCount.java
@@ -3,8 +3,10 @@ package com.example.carpoolapp;
 import android.content.Context;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -22,8 +24,8 @@ import static android.content.ContentValues.TAG;
 
 public class UpdateDriveCount {
 
-public ArrayList<ArrayList<User>> totalUsers =new ArrayList<>();
-public User curentUser;
+    public ArrayList<ArrayList<User>> totalUsers = new ArrayList<>();
+    public User curentUser;
 
     public void onCreate() {
 
@@ -41,26 +43,24 @@ public User curentUser;
         //  return;
 
         getusers(userId, new UserItemCallBack() {
-                    @Override
-                    public void OnCallback(ArrayList<User> itemList) {
+            @Override
+            public void OnCallback(ArrayList<User> itemList) {
 
-                        totalUsers.add(itemList);
+                totalUsers.add(itemList);
 
-                        if(totalUsers.size() == curentUser.carPools.size())//finished geting users
+                if (totalUsers.size() == curentUser.carPools.size())//finished geting users
+                {
+                    int stopint = 0;
+                    for (ArrayList<User> list : totalUsers) {
+                        sortBasedOnDriveCount(list);
+
+                        if (curentUser.id.equals(list.get(0).id))//if curentuser has the lowest drive count
                         {
-                            int stopint = 0;
-                            for (ArrayList<User> list:totalUsers) {
-                                sortBasedOnDriveCount(list);
-
-                                if( curentUser.id.equals(list.get(0).id ))//if curentuser has the lowest drive count
-                                {
-                                    IncromentDriveCount();
-                                }
-                            }
-
-
+                            IncromentDriveCount(list.get(0));
                         }
                     }
+                }
+            }
 
             @Override
             public void finished() {
@@ -70,22 +70,36 @@ public User curentUser;
     }
 
 
-    private void IncromentDriveCount() {
+    private void IncromentDriveCount(User user) {
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("CarPools").document(user.carPools.get(0)).collection(user.id).document(user.id)
+                .update("driveCount", user.driveCount + 1)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 
     private void sortBasedOnDriveCount(ArrayList<User> list) {
-        Collections.sort(list, new Comparator <User>() {
+        Collections.sort(list, new Comparator<User>() {
             @Override
             public int compare(User user, User t1) {
-                if(user.driveCount == t1.driveCount) return user.firstName.compareTo(t1.firstName);
+                if (user.driveCount == t1.driveCount) return user.firstName.compareTo(t1.firstName);
 
                 return Integer.compare(user.driveCount, t1.driveCount);
             }
         });
     }
-    private void getusers(String userId, UserItemCallBack callBack)
-    {
+
+    private void getusers(String userId, UserItemCallBack callBack) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("users").document(userId)
                 .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -110,13 +124,12 @@ public User curentUser;
                                     @Override
                                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                                         User user = documentSnapshot.toObject(User.class); // get user info
-                                       // userIDMap.get("0");
+                                        // userIDMap.get("0");
 
                                         user.carPools.add((String) userIDMap.get("0"));
                                         itemArrayList.add(user);
 
-                                        if(itemArrayList.size() == userIDMap.size()-1)
-                                        {
+                                        if (itemArrayList.size() == userIDMap.size() - 1) {
                                             callBack.OnCallback(itemArrayList);
                                         }
 
@@ -147,9 +160,10 @@ public User curentUser;
             }
         });
     }
-    private interface UserItemCallBack
-    {
-        void OnCallback(  ArrayList<User> itemList );
+
+    private interface UserItemCallBack {
+        void OnCallback(ArrayList<User> itemList);
+
         void finished();
 
     }
